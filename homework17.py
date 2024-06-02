4c2210f9ae1941f92ff3c3eaa76e3f62457da981
Morff3 <166973084+Morff3@users.noreply.github.com>
2024-06-02 14:46:25 +0700

    Update homework17.py

--- homework17.py
+++ homework17.py
@@ -4,7 +4,8 @@ class Buiding:
     def __init__(self):
         Buiding.total += 1
 
-buiding = Buiding()
 
-for buiding.total in range(41):
+building = Buiding()
+
+for building.total in range(41):
     print(Buiding.total)
